97072edcc21db8278f8a8b38f7479836b9689e73
Shakhzod <shaxzodusmanov11@gmail.com>
2026-02-22 00:33:12 +0500

    hero component created (not finished)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   )
 }
