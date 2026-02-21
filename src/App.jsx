60b5fac7c013ab0ef6d005dc213145ae8a30ca28
Shakhzod <shaxzodusmanov11@gmail.com>
2026-02-22 01:16:12 +0500

    fix animation and add video with scroll trigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
